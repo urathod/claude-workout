91e3d774d719b18577561b0810f33abe2305aff4
urathod <uttamrathod@gmail.com>
2026-02-16 09:49:42 -0800

    initial setup

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,11 @@
-import { defineConfig } from "drizzle-kit";
-import * as dotenv from "dotenv";
-
-dotenv.config({ path: ".env" });
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  schema: "./src/db/schema.ts",
-  out: "./drizzle",
-  dialect: "postgresql",
+  out: './drizzle',
+  schema: './src/db/schema.ts',
+  dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
-  verbose: true,
-  strict: true,
 });
